1a3a6c10339d0422862209e2348a616486f6c389
Linusp <linusp1024@gmail.com>
2020-07-22 09:46:44 +0800

    optimize `extract_text`

--- inoreader/utils.py
+++ inoreader/utils.py
@@ -18,4 +18,20 @@ def extract_text(html_content):
         return html_content
 
     content = html.fromstring(html_content)
+    for img in content.iter('img'):
+        img_src = img.get('src')
+        img_alt = img.get('alt') or img_src
+        if not img_src:
+            continue
+
+        img.text = '![%s](%s)' % (img_alt, img_src)
+
+    for link in content.iter('a'):
+        url = link.get('href')
+        text = link.text or url
+        if not url:
+            continue
+
+        link.text = '[%s](%s)' % (text, url)
+
     return content.text_content().replace('\xa0', '').strip()
